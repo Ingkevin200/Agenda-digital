42fc3c5aa159b38bc774a2fbd8a3053a8011368b
Ingkevin200 <kalgomez@poligran.edu.co>
2025-06-09 23:52:40 -0500

    actua

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Mejora: incluir el parámetro `Key`
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Digital Agenda')),
-        body:
-            Center(child: Text('Hola a tu agenda digital desde el frontend!')),
+        appBar: AppBar(title: const Text('Digital Agenda')), // Mejora: usar `const`
+        body: const Center(
+          child: Text('Hola a tu agenda digital desde el frontend!'), // Mejora: usar `const`
+        ),
       ),
     );
   }
 }
+
